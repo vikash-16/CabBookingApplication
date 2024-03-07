e76794ea581e98786db8a87a1b1a53994d463866
vikash-16 <68826409+vikash-16@users.noreply.github.com>
2024-03-07 12:59:15 +0530

    initial

---
+++ src/main/java/com/example/CabBookingApplication/CabBookingApplication.java
@@ -0,0 +1,14 @@
+package com.example.CabBookingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBookingApplication.class, args);
+		System.out.println("Cab Booking Application");
+	}
+
+}
